451eeee63d4dd927a6dfc64d2f3a93a5133e579d
Mariana <nanomarii10@gmail.com>
2022-06-15 20:58:09 -0300

    Exercício prático parte 1 - alteração de var para let e modificando a estrutura da função para arrow function

---
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/arrowfunctions.js
@@ -0,0 +1,26 @@
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+
+//   testingScope(true);
+
+  // Modificando a estrutura da função para arrow function.
+  const testingScope = (escopo) => {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+        console.log(ifScope);
+      } else {
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+      }
+  }
+
+  testingScope(true);
