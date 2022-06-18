0d7ab91ef1083d61d5c36ad7dd4bae497ddce68c
Mariana <nanomarii10@gmail.com>
2022-06-17 21:28:46 -0300

    Testando a função sum e iniciando o teste de erro com mensagem

--- dia-3-primeiros-passos-em-jest/sum.js
+++ dia-3-primeiros-passos-em-jest/sum.js
@@ -1,7 +1,9 @@
-module.exports = sum;
+function sum(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('parameters must be numbers');
+    }
 
-const sum = (a, b) => {
     return a + b;
 }
 
-sum(4, 5);
+module.exports = sum;
--- dia-3-primeiros-passos-em-jest/sum.test.js
+++ dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,1 +1,8 @@
 const sum = require('./sum.js'); 
+
+test('add 4+5 to equal 9', () => {
+    expect(sum(4,5)).toBe(9)
+})
+test('add 0+0 to equal 0', () => {
+    expect(sum(0,0)).toBe(0)
+})
