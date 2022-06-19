9cc5092867e65e30b6e4ccd84223e21b1b483658
Mariana <nanomarii10@gmail.com>
2022-06-19 18:31:00 -0300

    Pasta gitignore criada e alterando test por it

--- dia-3-primeiros-passos-em-jest/sum.test.js
+++ dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,8 +1,8 @@
-const sum = require('./sum.js'); 
+const sum = require('./sum'); 
 
-test('add 4+5 to equal 9', () => {
+it('add 4+5 to equal 9', () => {
     expect(sum(4,5)).toBe(9)
 })
-test('add 0+0 to equal 0', () => {
+it('add 0+0 to equal 0', () => {
     expect(sum(0,0)).toBe(0)
 })
