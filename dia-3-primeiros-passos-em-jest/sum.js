469d6293cbe755cba73e9e093b43e2f64c119b1f
Mariana <nanomarii10@gmail.com>
2022-06-19 19:48:35 -0300

    Inserindo a estrutura da function myRemove e fazendo a expostação/importação para o arquivo de teste

--- dia-3-primeiros-passos-em-jest/sum.js
+++ dia-3-primeiros-passos-em-jest/sum.js
@@ -1,3 +1,4 @@
+// A função sum(a, b) retorna a soma do parâmetro a com o b.
 function sum(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new Error('parameters must be numbers');
@@ -6,4 +7,17 @@ function sum(a, b) {
     return a + b;
 }
 
-module.exports = sum;
+//A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array.
+function myRemove(arr, item) {
+    let newArr = [];
+    for (let index = 0; index < arr.length; index += 1) {
+      if (item !== arr[index]) {
+        newArr.push(arr[index]);
+      }
+    }
+    return newArr;
+  }
+  
+  // implemente seus testes aqui
+
+module.exports = {sum, myRemove};
--- dia-3-primeiros-passos-em-jest/sum.test.js
+++ dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum'); 
+const {sum, myRemove} = require('./sum'); 
 
 it('add 4+5 to equal 9', () => {
     expect(sum(4,5)).toBe(9)
